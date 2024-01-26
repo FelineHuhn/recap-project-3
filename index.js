0646176464ff9813d48cf727e9bc15d2f7ecb6be
Feline Huhn <feline.huhn@gmx.de>
2024-01-26 13:09:11 +0100

    add fetch data logic to display character card for each fetched character

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,22 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  cardContainer.innerHTML = "";
+
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    const characters = data.results;
+
+    characters.forEach((character) => {
+      const characterCard = CharacterCard(character);
+      cardContainer.append(characterCard);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchCharacters();
